dc53b3aabbd26e7cfbfe23e42d951ac87dc7e9a1
dambolewski <dambolewski@gmail.com>
2021-03-26 17:59:36 +0100

    One to one relation

---
+++ src/main/java/pl/javastart/model/UserDetails.java
@@ -0,0 +1,79 @@
+package pl.javastart.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_details")
+public class UserDetails implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user_details")
+    private long id;
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+    private String address;
+    @OneToOne(mappedBy = "details")
+    private User user;
+
+    public UserDetails(String firstName, String lastName, String address) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public UserDetails() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDetails [id=" + id + ", firstName=" + firstName + ", "
+                + "lastName=" + lastName + ", address=" + address + "]";
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
