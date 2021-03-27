1ffa93de65081a556a6396147dcf934822d5dcd1
dambolewski <dambolewski@gmail.com>
2021-03-27 17:51:18 +0100

    Cascade Operations

--- src/main/java/pl/javastart/dao/ClientDao.java
+++ src/main/java/pl/javastart/dao/ClientDao.java
@@ -8,4 +8,9 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 public class ClientDao extends GenericDao<Client, Long>{
+
+    public void removeAllOrders(Client client){
+        Client managedClient = get(client.getId());
+        managedClient.getOrders().clear();
+    }
 }
--- src/main/java/pl/javastart/model/Order.java
+++ src/main/java/pl/javastart/model/Order.java
@@ -1,5 +1,8 @@
 package pl.javastart.model;
 
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +15,15 @@ public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id_order")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order")
     private Long id;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER,
+            cascade = CascadeType.PERSIST)
+    @Fetch(FetchMode.SELECT)
     @JoinTable(name = "order_products",
-            joinColumns = {@JoinColumn(name="order_id", referencedColumnName="id_order")},
-            inverseJoinColumns = {@JoinColumn(name="product_id", referencedColumnName="id_product")}
-    )
+            joinColumns = {@JoinColumn(name = "order_id", referencedColumnName = "id_order")},
+            inverseJoinColumns = {@JoinColumn(name = "product_id", referencedColumnName = "id_product")})
     private List<Product> products = new ArrayList<>();
     @Column(name = "details", length = 512)
     private String orderDetails;
@@ -33,9 +37,6 @@ public class Order implements Serializable {
     public Order(String orderDetails) {
         this.orderDetails = orderDetails;
     }
-
-    //settery, gettery
-
 
     public Long getId() {
         return id;
